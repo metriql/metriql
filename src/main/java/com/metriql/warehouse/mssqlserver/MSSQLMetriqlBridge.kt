7734ca14d44c2f89f3cedb5403b337068080d710
buremba <emrekabakci@gmail.com>
2021-08-20 09:04:58 +0300

    Fix SQL Server limit issue

--- src/main/java/com/metriql/warehouse/mssqlserver/MSSQLMetriqlBridge.kt
+++ src/main/java/com/metriql/warehouse/mssqlserver/MSSQLMetriqlBridge.kt
@@ -22,7 +22,11 @@ object MSSQLMetriqlBridge : ANSISQLMetriqlBridge() {
         ServiceType.SEGMENTATION to object : ANSISQLSegmentationQueryGenerator() {
             override fun getMap(context: IQueryGeneratorContext, queryDSL: Segmentation): Map<String, Any?> {
                 // MsSQL doesn't support GROUP BY 1,2,3
-                return super.getMap(context, queryDSL) + mapOf("groups" to queryDSL.groups)
+                return super.getMap(context, queryDSL) + mapOf("groups" to queryDSL.groups) + if(queryDSL.limit != null) {
+                    mapOf("limit" to null)
+                } else {
+                    mapOf()
+                }
             }
         },
         ServiceType.FUNNEL to ANSISQLFunnelQueryGenerator()
