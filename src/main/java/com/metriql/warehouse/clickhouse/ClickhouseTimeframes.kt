2c7cb08bbe5a12b207bfb7f504e4fb186875383f
buremba <emrekabakci@gmail.com>
2021-09-14 23:50:46 +0300

    Handle NULL correctly in Clickhouse timeframes

--- src/main/java/com/metriql/warehouse/clickhouse/ClickhouseTimeframes.kt
+++ src/main/java/com/metriql/warehouse/clickhouse/ClickhouseTimeframes.kt
@@ -5,14 +5,15 @@ import com.metriql.warehouse.spi.function.TimePostOperation
 import com.metriql.warehouse.spi.function.TimestampPostOperation
 import com.metriql.warehouse.spi.function.WarehouseTimeframes
 
+// CAST(val AS DATE) throws an exception is val is NULL
 class ClickhouseTimeframes : WarehouseTimeframes {
     override val timestampPostOperations: Map<TimestampPostOperation, String> = mapOf(
-        TimestampPostOperation.HOUR to "CAST(DATE_TRUNC('hour', %s) AS TIMESTAMP)",
-        TimestampPostOperation.DAY to "CAST(DATE_TRUNC('day', %s) AS DATE)",
-        TimestampPostOperation.WEEK to "CAST(DATE_TRUNC('week', %s) AS DATE)",
-        TimestampPostOperation.MONTH to "CAST(DATE_TRUNC('month', %s) AS DATE)",
-        TimestampPostOperation.QUARTER to "CAST(DATE_TRUNC('quarter', %s) AS DATE)",
-        TimestampPostOperation.YEAR to "CAST(DATE_TRUNC('year', %s) AS DATE)",
+        TimestampPostOperation.HOUR to "CAST(DATE_TRUNC('hour', %s), 'Nullable(datetime)')",
+        TimestampPostOperation.DAY to "CAST(DATE_TRUNC('day', %s), 'Nullable(date)')",
+        TimestampPostOperation.WEEK to "CAST(DATE_TRUNC('week', %s), 'Nullable(date)')",
+        TimestampPostOperation.MONTH to "CAST(DATE_TRUNC('month', %s), 'Nullable(date)')",
+        TimestampPostOperation.QUARTER to "CAST(DATE_TRUNC('quarter', %s), 'Nullable(date)')",
+        TimestampPostOperation.YEAR to "CAST(DATE_TRUNC('year', %s), 'Nullable(date)')",
         TimestampPostOperation.HOUR_OF_DAY to "formatDateTime(%s, '%%H:%%M')",
         TimestampPostOperation.DAY_OF_MONTH to "toDayOfMonth(%s)",
         TimestampPostOperation.WEEK_OF_YEAR to "toWeek(%s, 3)",
@@ -22,11 +23,11 @@ class ClickhouseTimeframes : WarehouseTimeframes {
     )
 
     override val datePostOperations: Map<DatePostOperation, String> = mapOf(
-        DatePostOperation.DAY to "CAST(%s AS DATE)",
-        DatePostOperation.WEEK to "CAST(DATE_TRUNC('week', %s) AS DATE)",
-        DatePostOperation.MONTH to "CAST(DATE_TRUNC('month', %s) AS DATE)",
-        DatePostOperation.QUARTER to "CAST(DATE_TRUNC('quarter', %s) AS DATE)",
-        DatePostOperation.YEAR to "CAST(DATE_TRUNC('year', %s) AS DATE)",
+        DatePostOperation.DAY to "CAST(%s, 'Nullable(date)')",
+        DatePostOperation.WEEK to "CAST(DATE_TRUNC('week', %s), 'Nullable(date)')",
+        DatePostOperation.MONTH to "CAST(DATE_TRUNC('month', %s), 'Nullable(date)')",
+        DatePostOperation.QUARTER to "CAST(DATE_TRUNC('quarter', %s), 'Nullable(date)')",
+        DatePostOperation.YEAR to "CAST(DATE_TRUNC('year', %s), 'Nullable(date)')",
         DatePostOperation.DAY_OF_MONTH to "EXTRACT(day FROM %s)",
         DatePostOperation.WEEK_OF_YEAR to "toISOWeek(%s)",
         DatePostOperation.MONTH_OF_YEAR to "toMonth(%s)",
