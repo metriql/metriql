b31701cebbb8f1155c649a952e7df75a0d5c8bcf
buremba <emrekabakci@gmail.com>
2021-11-29 16:00:37 +0300

    Add credential info to GDS screen

--- frontend/src/assets/docs/datastudio.vue
+++ frontend/src/assets/docs/datastudio.vue
@@ -3,10 +3,18 @@
     <li><a href="https://datastudio.google.com/datasources/create?connectorId=AKfycbw8o0F6LEr0epNSNVWqNzlqo7R-6jRYxxSxBspzyg2Xi6SDFItLN_aM3l_U56Z0obwS" target="_blank">Install metriql Connector</a> to your Data Studio account.</li>
     <li>
       <template v-if="!$BASE.IS_LOCAL">
-      Enter URL:
-      <el-tooltip  content="Click to copy" placement="top">
-        <button  @click="$clipboard($BASE.URL)" el-tooltip="Click to copy">{{$BASE.URL}}</button>
-      </el-tooltip>
+        <div>
+          Enter URL:
+          <el-tooltip  content="Click to copy" placement="top">
+            <button  @click="`${$clipboard($BASE.URL)}/api/v0`" el-tooltip="Click to copy">{{$BASE.URL}}/api/v0</button>
+          </el-tooltip>
+        </div>
+        <div v-if="isPasswordless">
+          Metriql is passwordless, feel free to enter random values for username & and password.
+        </div>
+        <div v-else>
+          Enter your username & password pair for Metriql.
+        </div>
       </template>
       <div v-else>
         Your server is in your local environment (<b>{{$BASE.URL}}</b>) so you need to expose your local port to the internet so that Google servers can access it.
@@ -22,3 +30,14 @@
     <li>You're done. 🎉</li>
   </ol>
 </template>
+<script>
+import { AuthService, AuthType } from '/src/services/auth'
+
+export default {
+  computed: {
+    isPasswordless: function() {
+      return AuthService.getAuth() == AuthType.NONE
+    }
+  }
+}
+</script>
