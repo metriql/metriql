8265f058258a210fdac2f9b9d5dca4df6a0657ef
Burak Emre Kabakcı <emrekabakci@gmail.com>
2022-07-18 19:35:11 +0100

    Add simple Clickhouse test

--- src/test/java/com/metriql/warehouse/clickhouse/TestWarehouseClickhouse.kt
+++ src/test/java/com/metriql/warehouse/clickhouse/TestWarehouseClickhouse.kt
@@ -1,8 +1,14 @@
 package com.metriql.warehouse.clickhouse
 
 import com.metriql.tests.JdbcTestWarehouse
+import org.testng.annotations.Test
 
 class TestWarehouseClickhouse : JdbcTestWarehouse(useIntsForBoolean = true) {
     override val testingServer = TestingEnvironmentClickhouse
     override val tableDefinition = "ENGINE = Memory"
+
+    @Test
+    fun testName() {
+        testingServer.runQueryFirstRow("select 1")
+    }
 }
