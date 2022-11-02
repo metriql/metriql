5686a1450c76477b9c481397e17e95c3279cf697
Burak Emre Kabakcı <emrekabakci@gmail.com>
2022-11-02 23:58:40 +0000

    Update UnirestHelper.kt

--- src/main/java/com/metriql/util/UnirestHelper.kt
+++ src/main/java/com/metriql/util/UnirestHelper.kt
@@ -1,13 +1,20 @@
 package com.metriql.util
 
 import com.fasterxml.jackson.core.JsonProcessingException
+import kong.unirest.Config
 import kong.unirest.GenericType
+import kong.unirest.HttpRequest
+import kong.unirest.HttpRequestSummary
+import kong.unirest.HttpResponse
+import kong.unirest.Interceptor
 import kong.unirest.ObjectMapper
 import kong.unirest.Unirest
 import java.io.IOException
+import java.util.logging.Logger
 
 object UnirestHelper {
     val unirest = Unirest.spawnInstance()!!
+    private val logger = Logger.getLogger(this::class.java.name)
 
     init {
         Runtime.getRuntime().addShutdownHook(object : Thread() {
@@ -21,7 +28,7 @@ object UnirestHelper {
             .connectTimeout(1_200_000)
             .socketTimeout(2_800_000)
             /** Setting the object mapper is required to use our own helpers and annotations */
-            .objectMapper = object : ObjectMapper {
+            .setObjectMapper( object : ObjectMapper {
             private val mapper = JsonHelper.getMapper().copy()
 
             override fun <T> readValue(value: String, valueType: Class<T>): T {
@@ -47,6 +54,14 @@ object UnirestHelper {
                     throw RuntimeException(e)
                 }
             }
-        }
+        }).interceptor(object : Interceptor {
+                override fun onRequest(request: HttpRequest<*>, config: Config?) {
+                    logger.info("Sending request to ${request.url}")
+                }
+
+                override fun onResponse(response: HttpResponse<*>, request: HttpRequestSummary, config: Config?) {
+                    //
+                }
+            })
     }
 }
